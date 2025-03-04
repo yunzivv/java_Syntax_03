86e34403fc0e541a6cf0aca847849b351abf5fc4
yunzivv <yunzivv@gmail.com>
2025-03-04 10:45:19 +0900

    변수 a와 b의 값 바꾸기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,34 @@
 package org.example;
 
 
-// 문제 : `안녕하세요.`를 10번 출력해주세요.
+// 문제 : a와 b가 가지고 있는 값을 서로 뒤바꿔주세요.
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("커밋테스트 2");
+        // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
+        // `int` => a라는 공간에는 정수만 담을 수 있다.
+        // `;` => 문장이 끝남을 알린다.
+        int a;
+        a = 5;
+
+        System.out.println(a);
+        System.out.println(a + 10);
+
+        int b = 10;
+
+        // `+` => 문장과 문자을 합친다.
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+        // 여기서 부터
+        int c = a;
+        a = b;
+        b = c;
+        // 여기까지 수정 가능
+
+        System.out.println("a : " + a);
+        // 출력 : a : 10
+        System.out.println("b : " + b);
+        // 출력 :  b : 5
     }
 }
