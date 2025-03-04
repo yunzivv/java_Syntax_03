5cc7fc984d43a001fd2e3cf053c3dffbac721f28
yunzivv <yunzivv@gmail.com>
2025-03-04 15:56:11 +0900

    If-ElseIf-Else

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,113 @@
 package org.example; // 없으면 안됨
 
 
-// 문제 : 실행되는 출력문에는 참 그렇지 않으면 거짓 이라고 적어주세요.
+// 문제 : 할인 대상인지 아닌지 출력해주세요.
+// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
+// 조건 : 출력예시 처럼 출력되어야 합니다.
+// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
+// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
+// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
 
 class Main {
     public static void main(String[] args) {
-        int age = 38;
+        int age = 60; // 이 값을 바꿔가면서 실행해보세요.
 
-        if (10 > age && age > 0){
-            System.out.println("영유아");
+        System.out.println("당신의 나이는 " + age + "살 입니다.");
+
+        // 구현시작
+        System.out.println("==== v1 ====");
+        if (age > 59 || age < 20){
+            System.out.println("할인대상");
+        } else {
+            System.out.println("할인대상 아님");
         }
-        else if (20 > age){ // 양립할 수 없음 -> 무조건 한가지만 참
-            System.out.println("10대");
+
+        System.out.println("==== v2 ====");
+        if (age > 19 && age < 60){
+            System.out.println("할인대상 아님");
+        } else {
+            System.out.println("할인대상");
         }
-        else if (30 > age){ // python의 elif와 같은 작동
-            System.out.println("20대");
+
+        System.out.println("==== v3 ====");
+        if ( age < 20 ){
+            System.out.println("할인대상");
+        } else if (age > 59){
+            System.out.println("할인대상");
+        } else {
+            System.out.println("할인대상 아님");
         }
-        else if (40 > age){
-            System.out.println("30대");
+
+        System.out.println("==== v4 ====");
+        if ( age < 20 ){
+            System.out.println("할인대상");
+        } else if (age > 59){
+            System.out.println("할인대상");
+        } else if (age > 19 && age < 60){
+            System.out.println("할인대상 아님");
         }
-        else {
-            System.out.println("40대 이상");
+
+        System.out.println("==== v5 ====");
+        if ( age < 20 ){
+            System.out.println("할인대상");
+        } else if (age > 59){
+            System.out.println("할인대상");
+        } else if (age < 60 && age > 19){
+            System.out.println("할인대상 아님");
         }
+
+        System.out.println("==== v6 ====");
+        if (age < 20 || age > 59){
+            System.out.println("할인대상");
+        } else if (age > 19 && age < 60){
+            System.out.println("할인대상 아님");
+        }
+
+        System.out.println("==== v7 ====");
+        if (age < 20 || age > 59){
+            System.out.println("할인대상");
+        }
+        if (age > 19 && age < 60){
+            System.out.println("할인대상 아님");
+        }
+
+
+        System.out.println("==== v8 ====");
+        if (age < 20){
+            System.out.println("할인대상");
+        }
+        if (age > 59){
+            System.out.println("할인대상");
+        }
+        if (age > 19 && age < 60){
+            System.out.println("할인대상 아님");
+        }
+
+        System.out.println("==== v9 ====");
+        if (age < 20){
+            System.out.println("할인대상");
+        }
+        if (age > 59){
+            System.out.println("할인대상");
+        }
+        if (age >= 20){
+            if(age <= 59){
+                System.out.println("할인대상 아님");
+            }
+        }
+
+        System.out.println("==== v10 ====");
+        if (age > 59) {
+            System.out.println("할인대상");
+        } else {
+            if(age < 20){
+                System.out.println("할인대상");
+            }
+            else{
+                System.out.println("할인대상 아님");
+            }
+        }
+
+        // 출력 => 할인대상입니다. 또는 할인 대상이 아닙니다.
     }
 }
