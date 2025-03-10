7b203ae124a5bb47b7f20e3cf8c1cf807e0867d5
yunzivv <yunzivv@gmail.com>
2025-03-10 09:53:41 +0900

    배열 기초

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,22 +9,27 @@ class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        boolean[] arr1 = new boolean[3]; // 바로 대입하는 법?
+        boolean[] arr1 = {true, false, true}; // 바로 대입하는 법?
+//        arr1[0] = true;
+//        arr1[1] = false;
+//        arr1[2] = true;
+        System.out.println(arr1); // arr1의 주소값 출력
+
+        arr1 = new boolean[3]; // 같은 변수에 새로운 배열 대입 가능
+
         double[] arr2 = new double[3];
-        int[] arr3 = new int[10];
-
-        arr1[0] = true;
-        arr1[1] = false;
-        arr1[2] = true;
-
         arr2[0] = 3.14;
         arr2[1] = 7.77;
         arr2[2] = 11.11;
 
-        for (int i = 0; i < arr3.length; i++){
-            arr3[i] = i + 1;
 
+        int[] arr3 = new int[10];
+        for (int i = 0; i < arr3.length; i++){ // i는 for문 탈출하면 죽음...rip
+            arr3[i] = i + 1; // 여기에 출력문 작성해도 됨
         }
+
+        int[] arr4 = new int[]{1,3,5,7};
+
 
         System.out.println("= arr1 출력 =");
         for (int i = 0; i < arr1.length; i++){
@@ -37,6 +42,10 @@ class Main {
         System.out.println("= arr3 출력 =");
         for (int i = 0; i < arr3.length; i++){
             System.out.println(arr3[i]);
+        }
+        System.out.println("= arr4 출력 =");
+        for (int i = 0; i < arr4.length; i++){
+            System.out.println(arr4[i]);
         }
     }
 
