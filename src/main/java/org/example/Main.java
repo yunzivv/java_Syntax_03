0bb44007eba55eecc2f275100246c4be8e8c1a16
yunzivv <yunzivv@gmail.com>
2025-03-11 13:11:29 +0900

    문제2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,53 +1,43 @@
 package org.example;
 
-// 문제 1 - 함수를 실행하면 값을 돌려주도록 만들어주세요. 250311
+// 문제 2 : 아래와 같이 출력 되도록 해주세요.
+
 class Main {
-//    import java.util.Scanner;
-
     public static void main(String[] args) {
-        int 결과;
+        // 예시 시작
+        int c = 20 + Math.더하기(10, 20);
+        System.out.println(c);
+        // 예시 끝
 
-        결과 = 계산기.합(10, 20);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 30
+        int 결과1 = Math.oneToSum(3);
+        System.out.println("결과1 : " + 결과1);
+        // 출력 : 결과1 : 6
 
-        결과 = 계산기.합(30, 20);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 50
-
-        결과 = 계산기.합(30, 70);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 100
-
-        결과 = 계산기.차(30, 70);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : -40
-
-        결과 = 계산기.곱(3, 7);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 21
-    }
-    class 계산기 {
-
-        static int 합(int a, int b) {
-            return a + b;
-        }
-
-        static int 차(int a, int b) {
-            return a - b;
-        }
-
-        static int 곱(int a, int b) {
-            return a * b;
-        }
-
-
+        int 결과2= Math.oneToSum(10);
+        System.out.println("결과2 : " + 결과2);
+        // 출력 : 결과2 : 55
     }
 }
 
+class Math {
+    // static => 본사직원
+    // static은 현재 클래스에 지금당장 존재한다.
+    static int 더하기(int a, int b) {
+        // 메인 메서드의 int c와 아래 int c는 전혀 관계가 없다.
+        int c = a + b;
 
+        // return 값의 타입과 함수명의 앞부분이 같아야 한다.
+        // return => 변신
+        return c;
+    }
 
-
-
-
-
+    static int oneToSum(int a) {
+        int sum = 0;
+        int i = 0;
+        while(i <= a){
+            sum += i;
+            i++;
+        }
+        return sum;
+    }
+}
