a51c428bee0301ff42c4fb9e6cfa1ee38376f618
yunzivv <yunzivv@gmail.com>
2025-03-11 13:05:25 +0900

    문제1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,48 @@
 package org.example;
 
-// return 250311
-
-import java.util.Scanner;
+// 문제 1 - 함수를 실행하면 값을 돌려주도록 만들어주세요. 250311
 class Main {
-
-    static int a = 50;
+//    import java.util.Scanner;
 
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in); // ctrl + shift + o -> 자동 import
-        System.out.println("== 1 ==");
-        cal.sum(10, 20);
-        System.out.println("== 2 ==");
-        System.out.println(cal.sum2(10, 20));
-//        System.out.println(cal.sum(10,20)); // return값이 없기 때문에 오류
+        int 결과;
+
+        결과 = 계산기.합(10, 20);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 30
+
+        결과 = 계산기.합(30, 20);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 50
+
+        결과 = 계산기.합(30, 70);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 100
+
+        결과 = 계산기.차(30, 70);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : -40
+
+        결과 = 계산기.곱(3, 7);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 21
+    }
+    class 계산기 {
+
+        static int 합(int a, int b) {
+            return a + b;
+        }
+
+        static int 차(int a, int b) {
+            return a - b;
+        }
+
+        static int 곱(int a, int b) {
+            return a * b;
+        }
+
 
     }
-
-}
-
-class cal {
-
-    // void는 return을 안할 의무가 있다.
-    static void sum(int a, int b) {  // void : output 하지 않는 함수
-        System.out.println(a + b); // 출력은 output 이 아님
-        return; // return을 하지 않겠다는 의미
-    }
-
-    // void를 제외한 나머지는 return을 할 의무가 있다. 무조건 return값을 가짐.
-    static int sum2(int a, int b) { // static 뒤에 반환값의 자료형을 작성
-        int c = a + b;
-        return 0; // 0 => return 밸류
-    }
-
 }
 
 
