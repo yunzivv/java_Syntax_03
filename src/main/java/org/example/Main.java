17e9a51046af1ca9d6789ad195a29754cc38cde9
yunzivv <yunzivv@gmail.com>
2025-03-11 16:48:03 +0900

    소수판별기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,47 +3,97 @@ package org.example;
 import java.awt.*;
 import java.util.Arrays;
 
-// 객체 활용과 풀이 250311
+// 소수판별기 250311
 
 class Main {
     public static void main(String[] args) {
-        String msg = greet("Jin");
-        // msg 변수에 greet 메서드를 실행한 return값을 담는다.
-        // 메서드에 넘겨줄 인수는 'Jin'이다
-        System.out.println(msg); // 예상 출력 : Hello, Jin!
+        boolean result = Math.isPrimeNumber(1);
+        System.out.println("1은(는) 소수인가? : " + result);
 
-        Point point = createPoint(5, 10); // = new Point(5, 10)
-        // Point 는 이미 있는 클래스 : 좌표를 나타냄
-        System.out.println("Point create at (" + point.x + ", " + point.y + ")");
+        result = Math.isPrimeNumber(2);
+        System.out.println("2은(는) 소수인가? : " + result);
 
-        int[] evens = firstNEvenNumbers(5);
-        // evens 배열에 firstNEvenNumbers 메서드를 실행한 return값을 담는다.
-        // 메서드에 넘겨줄 인수는 '5'다.
-        System.out.println(evens);
-        // 리모컨을 갖고 있기 때문에 주소 출력
-        System.out.println("first 5 even numbers : " + Arrays.toString(evens));
-        // Arrays 객체의 toSrting메서드를 활용해서 evens를 출력한다.
-        // toString메서드는 정수배열을 매개변수로 한다.
-    }
+        result = Math.isPrimeNumber(3);
+        System.out.println("3은(는) 소수인가? : " + result);
 
-    // 외부에서 사용 가능한 static 메서드
-    public static String greet(String name) { // 문자열 Jin을 매개변수로 받아 name변수로 활용한다.
-        return "Hello, " + name + "!";
-    }
+        result = Math.isPrimeNumber(4);
+        System.out.println("4은(는) 소수인가? : " + result);
 
-    public static Point createPoint(int x, int y) {
-        return new Point(x, y);
-    }
+        int rs = Math.one_to_n_prime_numbers_count(10);
+        System.out.println("rs : " + rs);
+        // rs : 4
+        rs = Math.one_to_n_prime_numbers_count(15);
+        System.out.println("rs : " + rs);
+        // rs :  6
 
-    public static int[] firstNEvenNumbers(int n) { // 정수 5를 매개변수로 받아 n변수로 활용한다.
-        int[] evenNumbers = new int[n]; // 정수배열 evenNumbers의 길이는 5다.
+        int countPrime = Math.count(15);
+        System.out.println(countPrime);
 
-        for (int i = 0, num = 2; i < n; i++, num += 2) {
-            // 정수형 변수 i와 num을 선언하고 for문 안에서 i는 0부터 n까지 즉, 5까지 1씩 증가하고,
-            // num은 i가 n보다 작은 동안 2부터 2씩 증가한다.
-            evenNumbers[i] = num; // evenNumbers배열에 순차적으로 2부터 짝수가 5개 저장된다.
-            // evenNumbers = {2, 4, 6, 8, 10}
-        }
-        return evenNumbers; // 저장된 evenNumbers 배열을 반환한다.
+
     }
 }
+
+class Math {
+    static boolean isPrimeNumber(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 1; i < n; ++i) {
+            if (i != 1 && n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    static int count(int i) {
+        int count = 0;
+
+        for(int j = 2; j <= i; ++j){
+            if(isPrimeNumber(j)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    static int one_to_n_prime_numbers_count(int n) {
+        int count = 0;
+
+        if (n > 2) {
+            count++;
+        }
+
+        for (int i = 2; i <= n; ++i) {
+
+            for (int j = 2; j < i; ++j) {
+
+                if (j == i - 1) {
+                    count++;
+                }
+                if (i % j == 0) {
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
