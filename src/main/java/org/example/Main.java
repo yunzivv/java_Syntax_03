7043f719e7f369f3028a88d11b8f7220bf8a93c7
yunzivv <yunzivv@gmail.com>
2025-03-11 12:16:07 +0900

    함수return

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,54 +1,37 @@
 package org.example;
 
-// 변수 - 전역변수, 지역변수, 매개변수  250311
+// return 250311
 
 import java.util.Scanner;
 class Main {
 
-    int aa = 30; //static이 아닌 전역변수
-    static int a = 50; // static 전역변수
+    static int a = 50;
 
     public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in); // ctrl + shift + o -> 자동 import
-        System.out.println(a); // a = 9행의 전역변수
-
-        int aa = 55;
-        System.out.println(aa); // a = 15행의 지역변수
-
-        ex();
-
-        cal.sum();
-
-        cal.sum(10, 20); // 괄호 안의 10, 20 인수(argument)을 sum 함수로 넘겨줌
-        cal.sum(30, 40); // 함수에서 받을 매개변수의 개수와 자료형을 맞춰줘야함, 순서대로 매치
+        System.out.println("== 1 ==");
+        cal.sum(10, 20);
+        System.out.println("== 2 ==");
+        System.out.println(cal.sum2(10, 20));
+//        System.out.println(cal.sum(10,20)); // return값이 없기 때문에 오류
 
     }
 
-    public static void ex() {
-        System.out.println(a); // a = 9행의 전역변수
-    }
 }
 
 class cal {
 
-    static void sum() {
-        System.out.println("a + b");
+    // void는 return을 안할 의무가 있다.
+    static void sum(int a, int b) {  // void : output 하지 않는 함수
+        System.out.println(a + b); // 출력은 output 이 아님
+        return; // return을 하지 않겠다는 의미
     }
 
-    static void sum(int a) {
-        System.out.println("a + b");
+    // void를 제외한 나머지는 return을 할 의무가 있다. 무조건 return값을 가짐.
+    static int sum2(int a, int b) { // static 뒤에 반환값의 자료형을 작성
+        int c = a + b;
+        return 0; // 0 => return 밸류
     }
-
-    static void sum(int a, int b) { // (int a, int b) => 매개변수(parameter), 함수 내부와 외부를 연결하는 매개체
-//        int a = 10; // 매개변수는 지역변수의 일종, a는 이미 선언되어 있음
-        System.out.println(a + b);
-    }
-
-    // sum()과 sum(int a, int b)의 함수명이 같음에도 오류가 나지 않는 이유
-    // 메서드 시그니처가 다르기 때문
-    // 메서드 시그니처 : 함수명 + 매개변수 (매개변수의 변수명은 관련 없음)
-
-    // 다형성(Polymorphism) 중에 오버로딩 상황이다
 
 }
 
