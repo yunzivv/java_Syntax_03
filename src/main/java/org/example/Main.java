7bda42144f313bcb420a31df158f2645f902bb15
yunzivv <yunzivv@gmail.com>
2025-03-11 13:14:23 +0900

    문제3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,24 @@
 package org.example;
 
-// 문제 2 : 아래와 같이 출력 되도록 해주세요.
+// 문제 3 : 아래와 같이 출력 되도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        // 예시 시작
-        int c = 20 + Math.더하기(10, 20);
-        System.out.println(c);
-        // 예시 끝
-
-        int 결과1 = Math.oneToSum(3);
+        int 결과1 = Math.nToMSum(2, 3);
         System.out.println("결과1 : " + 결과1);
-        // 출력 : 결과1 : 6
+        // 출력 : 결과1 : 5
 
-        int 결과2= Math.oneToSum(10);
+        int 결과2 = Math.nToMSum(5, 10);
         System.out.println("결과2 : " + 결과2);
-        // 출력 : 결과2 : 55
+        // 출력 : 결과2 : 45
     }
 }
 
 class Math {
-    // static => 본사직원
-    // static은 현재 클래스에 지금당장 존재한다.
-    static int 더하기(int a, int b) {
-        // 메인 메서드의 int c와 아래 int c는 전혀 관계가 없다.
-        int c = a + b;
-
-        // return 값의 타입과 함수명의 앞부분이 같아야 한다.
-        // return => 변신
-        return c;
-    }
-
-    static int oneToSum(int a) {
+    static int nToMSum(int a, int b){
         int sum = 0;
-        int i = 0;
-        while(i <= a){
+        for(int i = a; i <= b; ++i){
             sum += i;
-            i++;
         }
         return sum;
     }
