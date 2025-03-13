4412210d8ad2df22a9705df7e09b239fb88b515e
yunzivv <yunzivv@gmail.com>
2025-03-13 11:43:16 +0900

    객체리턴

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,42 +3,71 @@ package org.example;
 import java.awt.*;
 import java.util.Arrays;
 
-// 상속 250313
+// 객체 리턴 250313
 // 문제 : 자동차 리모콘이 페라리 객체를 가리키게 한 후
 // 해당 리모콘이 가리키고 있는 객체를 다시 페라리 리모콘으로 가리키게(참조하게) 하는 코드를 작성해주세요.
 
-class Main {
+
+//올바른 리턴타입으로 메서드를 만들어주세요
+public class Main {
     public static void main(String[] args) {
-        // 수정가능지역 시작
+        int i = 로봇.get정수();
+        boolean b = 로봇.get논리();
+        사람 a사람 = 로봇.get사람();
+        사람 a사람2 = 로봇.get사람2();
+        사람 a사람3 = 로봇.get사람3();
+        사람 a사람4 = 로봇.get사람4();
+        사람 a사람5 = 로봇.get사람5(123, false);
+        System.out.println(a사람5.number);
+        로봇.get사람6(123, false);
 
-        // case 1
-        자동차 자동차리모콘 = new 자동차();
-        페라리 페라리리모콘 = 자동차리모콘;
-        // 위 코드가 안되는 이유
-        // 만약 자동차 클래스를 상속받는 다른 현대클래스가 있을 경우
-        // 자동차리모콘이 현대클래스로 만든 객체를 갖고 있다면?
-
-        // case 2
-        // 이건 가능
-        자동차 자동차리모콘2 = new 페라리();
-        페라리 페라리리모콘2 = (페라리) 자동차리모콘2;
-
-        // case 3
-        // exception 오류(아마) 실행 후 런타임에서 오류 발생
-        // 컴파일러는 알지 못한다.
-        자동차 자동차리모콘3 = new 페라리();
-        현대 현대리모콘3 = (현대) 자동차리모콘2;
-
-        // 수정가능지역 끝
     }
+
+
 }
-class 자동차 {
-    void 달리다() {}
-    void 서다() {}
+
+class 로봇 {
+    static int get정수() {
+        return 0;
+    }
+
+    static boolean get논리() {
+        return false;
+    }
+
+    static 사람 get사람() { // 객체를 반환하는 함수
+        return null; // 기본타입 빼고 반환 가능
+    }
+
+    static 사람 get사람2() {
+        return new 사람(); // 객체를 만들어서 반환 가능
+    }
+
+    static 사람 get사람3() {
+        사람 a = null;
+        return a; //
+    }
+
+    static 사람 get사람4() {
+        사람 b = new 사람();
+        return b;
+    }
+
+    static 사람 get사람5(int i, boolean b) {
+        사람 c = new 사람();
+        c.number = i;
+        c.check = b;
+
+        return c;
+    }
+
+    static void get사람6(int i, boolean b) {
+        return; // void도 return 스테이트먼트를 가질 수 있지만 반환값은 없는 상태 -> 안해도 무방
+    }
+
 }
-class 페라리 extends 자동차 {
-    void 뚜껑이_열리다() {}
-}
-class 현대 extends 자동차 {
-    void 멋지다() {}
+
+class 사람 {
+    int number; // get사람5를 위해 작성
+    boolean check; // get사람5를 위해 작성
 }
