959791eeef93b3c6d3b48afd461e117b8157dbba
yunzivv <yunzivv@gmail.com>
2025-03-14 15:17:48 +0900

    ASCII Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// try-catch 250314
+// ASCII Code 250314
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,39 +8,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        int result = 계산기.나누기(10, 0);
-        System.out.println(result); // 이게 오류가 발생하면 이 다음의 코드가 실행이 안됨
-        System.out.println(1); // 이걸 실행시키기 위해 try catch 사용
+        System.out.println((int)'1');
+        System.out.println(isHanguel(' '));
+        System.out.println(isHanguel('안'));
+        System.out.println(isHanguel('녕'));
+        System.out.println(isHanguel('하'));
+        System.out.println(isHanguel('세'));
+        System.out.println(isHanguel('요'));
+        System.out.println(isHanguel('.'));
 
+    }
+    static boolean isHanguel(char c){
+
+        return c >= '가' && c <= '힣'; // 44032 ~ 55204 총 11172개
     }
 }
 
-// case 1
-class 계산기 {
-    static int 나누기(int a, int b) {
-
-        int rs = 0;
-
-        try {
-            rs = a / b;
-            return rs;
-        } catch (ArithmeticException e){
-            System.out.println("나누기 할 수 없어");
-            return 0;
-        } finally {
-            System.out.println("어쨋든 하긴햇어..");
-        }
-    }
-}
-
-// case 2
-class 계산기1 {
-
-    // 함수를 호출할 때 주의할 사항을 throw 오류상황 / 이렇게 작성
-    static int 나누기(int a, int b) throws ArithmeticException{ // 개조심
-        int rs = 0;
-
-        rs = a / b;
-        return rs;
-    }
-}
