cec640e7e540f9c1c8ea96cc3a67b0037ab09c6e
yunzivv <yunzivv@gmail.com>
2025-03-14 16:57:00 +0900

    종합문제 푸는 중

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,84 @@
 package org.example;
 
-// Object 클래스 - toString 250314
+// 종합문제 푸는 중 250314
+// Array List가 3개나 존재할 필요 없다.
+// 수정해보기
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+// 문제 : 아래가 실행되도록 해주세요.
+
+class Main {
     public static void main(String[] args) {
+        사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
-        Person a = new Person("케로로", 10);
-        Person b = new Person("쿠루루", 10);
+        a사람인력관리소.add사람("홍길순", 33);
+        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
+        a사람인력관리소.add사람("홍길동", 20);
+        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
+        a사람인력관리소.add사람("임꺽정", 30);
+        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
+        a사람인력관리소.add사람("케로로", 10);
 
-        System.out.println(a); // override 된 메서드가 자동 실행됨
-        System.out.println(b);
 
-        System.out.println("=========================");
-        System.out.println(a.toString());
-        System.out.println(b.toString());
+        사람 a사람1 = a사람인력관리소.get사람(1);
+        a사람1.자기소개();
+        // 저는 1번, 홍길순, 33살 입니다.
 
+        사람 a사람2 = a사람인력관리소.get사람(2);
+        a사람2.자기소개();
+        // 저는 2번, 홍길동, 20살 입니다.
+
+        사람 a사람3 = a사람인력관리소.get사람(3);
+        a사람3.자기소개();
+        // 저는 3번, 임꺽정, 30살 입니다.
+
+        사람 a사람4 = a사람인력관리소.get사람(4);
+        a사람4.자기소개();
     }
 }
 
-class Person extends Object { // Object는 최상위 클래스라서 extends가 가능
-    String name;
-    int age ;
+class 사람인력관리소 {
 
-    Person(String name, int age) {
+    static int num = 0;
+    static String name;
+    static int age;
+    static ArrayList<Integer> numbers = new ArrayList<>();
+    static ArrayList<String> names = new ArrayList<>();
+    static ArrayList<Integer> ages = new ArrayList<>();
+
+
+    void add사람(String name, int age){
+
+        num++;
+        numbers.add(num);
+        names.add(name);
+        ages.add(age);
+
+        System.out.printf("나이가 %d살인 %d번째 사람(%s)이 추가되었습니다.\n", age, num, name);
+    }
+
+    사람 get사람(int i) {
+        i -= 1;
+        return new 사람(numbers.get(i), names.get(i), ages.get(i));
+    }
+}
+
+class 사람 {
+
+    private int num;
+    private String name;
+    private int age;
+
+    사람(int num, String name, int age){
+        this.num = num;
         this.name = name;
         this.age = age;
     }
 
-    @Override // 오버라이딩임을 알려줌. 주석과 같다.
-    public String toString() {
-        return "이름 : " +  name + ", 나이 : " + age;
+    void 자기소개() {
+        System.out.printf("저는 %d번, %s, %d살입니다.\n", this.num, this.name, this.age);
     }
-}
 
+}
