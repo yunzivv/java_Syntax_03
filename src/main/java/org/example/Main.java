2f550991ae2a38c1a91ef21ea35786e3cebd32c0
yunzivv <yunzivv@gmail.com>
2025-03-14 14:02:52 +0900

    Array list2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,95 @@
 package org.example;
 
 
-
-// ArrayList 250314
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        List<Number> l = new ArrayList<>(); // 동적 할당(dynamic allocation)
-        l.add(10);
-        l.add(20);
-        l.add(30);
-        l.add(40);
-        l.add(50);
-        System.out.println(l);
-        System.out.println("====================");
-
-        l.remove(3);
-        System.out.println(l); // [10, 20, 30, 50]
-
+        v4();
     }
 
+    static void v4() {
+        System.out.println("==v4==");
+        List articles = new ArrayList<>();
+        articles.add(new Article()); // index : 0 , id : 1
+        articles.add(new Article()); // index : 1 , id : 2
+        articles.add(new Article()); // index : 2 , id : 3
+
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = (Article) articles.get(i);
+            System.out.println(article.id);
+        }
+    }
+
+    static void v3() {
+        System.out.println("==v3==");
+        ArrayList articles = new ArrayList<>();
+        articles.add(new Article()); // index : 0 , id : 1
+        articles.add(new Article()); // index : 1 , id : 2
+        articles.add(new Article()); // index : 2 , id : 3
+        articles.add(new Article()); // index : 3 , id : 4
+
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = (Article) articles.get(i);
+            System.out.println(article.id);
+        }
+    }
+
+    static void v2() {
+        System.out.println("==v2==");
+        ArrayList articles = new ArrayList();
+        articles.add(new Article()); // index : 0 , id : 1
+        articles.add(new Article()); // index : 1 , id : 2
+        articles.add(new Article()); // index : 2 , id : 3
+        articles.add(new Article()); // index : 3 , id : 4
+
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = (Article) articles.get(i);
+            System.out.println(article.id);
+        }
+    }
+
+    static void v1() {
+        System.out.println("==v1==");
+        Article[] articles = new Article[100];
+
+        int articlesSize = 0;
+
+        articles[0] = new Article();
+        articlesSize++;
+        articles[1] = new Article();
+        articlesSize++;
+        articles[2] = new Article();
+        articlesSize++;
+        articles[3] = new Article();
+        articlesSize++;
+        articles[4] = new Article();
+        articlesSize++;
+
+        for (int i = 0; i < articlesSize; i++) {
+            System.out.println(articles[i].id);
+        }
+    }
 }
 
+class Article {
+    static int lastId;
+
+    int id;
+    String regDate;
+
+    static {
+        lastId = 0;
+    }
+
+    Article() {
+        this(lastId + 1, "2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
+        lastId++;
+    }
+
+    Article(int id, String regDate) {
+        this.id = id;
+        this.regDate = regDate;
+    }
+}
