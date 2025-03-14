e835348a41b4406f1900d01847954131c6ca7684
yunzivv <yunzivv@gmail.com>
2025-03-14 15:02:24 +0900

    try-catch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// private 250314
+// try-catch 250314
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,41 +8,39 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        사람 a사람 = new 사람();
-//        a사람.id = 20;
-//        System.out.println(a사람.id);
+        int result = 계산기.나누기(10, 0);
+        System.out.println(result); // 이게 오류가 발생하면 이 다음의 코드가 실행이 안됨
+        System.out.println(1); // 이걸 실행시키기 위해 try catch 사용
 
-        // private 변수에 접근하는 방법
-        //
-        a사람.setId(3);
-        System.out.println("번호 : " + a사람.getId());
     }
 }
 
-class 사람 {
-    // private : 접근제한자 중 하나, 내부에서만 접근할 수 있음
-    // 외부에서 직접접근 불가 -> 간접 접근 가능
-    // 클래스의 필드는 private로 작성하는 게 관례
-    private int id = 11;
-    private int age;
+// case 1
+class 계산기 {
+    static int 나누기(int a, int b) {
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+        int rs = 0;
 
-    public int getAge() {
-        return age;
-    }
-
-    // 우클릭 -> Generate -> getter, setter, constructor 알아서 만들어 주기도 함
-    // getter
-    public int getId() {
-        return id;
-    }
-
-    // setter
-    public void setId(int id) {
-         this.id = id;
+        try {
+            rs = a / b;
+            return rs;
+        } catch (ArithmeticException e){
+            System.out.println("나누기 할 수 없어");
+            return 0;
+        } finally {
+            System.out.println("어쨋든 하긴햇어..");
+        }
     }
 }
 
+// case 2
+class 계산기1 {
+
+    // 함수를 호출할 때 주의할 사항을 throw 오류상황 / 이렇게 작성
+    static int 나누기(int a, int b) throws ArithmeticException{ // 개조심
+        int rs = 0;
+
+        rs = a / b;
+        return rs;
+    }
+}
