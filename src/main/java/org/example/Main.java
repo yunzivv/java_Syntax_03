4d013b12db3cb0b25234c4dfb3b8776022e19a43
yunzivv <yunzivv@gmail.com>
2025-03-14 11:02:22 +0900

    Array list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,28 @@
 package org.example;
 
-import java.awt.*;
-import java.util.Arrays;
 
-// 객체배열 250314
+
+// ArrayList 250314
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = new int[10];
+        List<Number> l = new ArrayList<>(); // 동적 할당(dynamic allocation)
+        l.add(10);
+        l.add(20);
+        l.add(30);
+        l.add(40);
+        l.add(50);
+        System.out.println(l);
+        System.out.println("====================");
 
-        Article[] articles = new Article[10];
+        l.remove(3);
+        System.out.println(l); // [10, 20, 30, 50]
 
-        Article article1 = new Article();
-        articles[0] = article1;
-
-        Article article2 = new Article();
-        articles[1] = article2;
-
-        System.out.println(articles[0].id); // 배열 -> 리모콘 -> 객체
-        System.out.println(articles[1].id);
-
-        System.out.println(article1.id); // 리모콘 -> 객체
-        System.out.println(article2.id);
     }
+
 }
 
-class Article {
-    static int lastId;
-
-    int id;
-    String regDate;
-
-    static {
-        lastId = 0;
-    }
-
-    Article() {
-        this(lastId + 1, "2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
-        lastId++;
-    }
-
-    Article(int id, String regDate) {
-        this.id = id;
-        this.regDate = regDate;
-    }
-}
