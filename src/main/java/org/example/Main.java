6702251c6211b8a437dce577a4a8573a267bd2a8
yunzivv <yunzivv@gmail.com>
2025-03-17 15:21:59 +0900

    백준10950

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 
 package org.example;
 
-// 백준 2739 250317
+// 백준 10950 250317
 
 import java.util.Scanner;
 
@@ -9,11 +9,18 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int dan = sc.nextInt();
+        int testcase = sc.nextInt();
+        int[] numbers = new int[testcase * 2];
+        int sum;
+        int lastindex = 0;
 
-        for (int i = 1; i < 10; i++){
-            System.out.printf("%d * %d = %d\n", dan, i , dan*i);
+        for (int i = 0; i < testcase * 2; i++) {
+            numbers[i] = sc.nextInt();
         }
-
+        for (int count = 0; count < testcase; count++) {
+            sum = numbers[lastindex] + numbers[lastindex + 1];
+            System.out.println(sum);
+            lastindex += 2;
+        }
     }
 }
