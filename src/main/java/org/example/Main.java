8a51458b85572bf9df2be5c4aa22a263f59bf242
yunzivv <yunzivv@gmail.com>
2025-03-17 16:07:54 +0900

    백준11021

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 
 package org.example;
 
-// 백준 25314 250317
+// 백준 11021 250317
 
 import java.util.Scanner;
 
@@ -9,12 +9,18 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int fourLong =  sc.nextInt();
-        int count = fourLong/4;
+        int testcase = sc.nextInt();
+        int[] numbers = new int[testcase * 2];
+        int sum;
+        int lastindex = 0;
 
-        for (int i = 0; i < count; i++){
-            System.out.print("long ");
+        for (int i = 0; i < testcase * 2; i++) {
+            numbers[i] = sc.nextInt();
         }
-        System.out.print("int");
+        for (int count = 0; count < testcase; count++) {
+            sum = numbers[lastindex] + numbers[lastindex + 1];
+            System.out.printf("Case #%d: %d\n", count + 1, sum);
+            lastindex += 2;
+        }
     }
 }
