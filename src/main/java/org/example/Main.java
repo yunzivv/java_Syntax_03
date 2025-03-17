c71017a2f60770f66480a0c402b5ba1bea632d6a
yunzivv <yunzivv@gmail.com>
2025-03-17 12:23:50 +0900

    백준1001

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,12 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello World!");
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A - B);
 
     }
 }
