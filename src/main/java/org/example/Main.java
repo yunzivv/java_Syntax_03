6dee26d19f9714d88b99cd7505fcc986e7c54903
yunzivv <yunzivv@gmail.com>
2025-03-17 14:50:51 +0900

    백준2739

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 
 package org.example;
 
-// 백준 2480 250317
+// 백준 2739 250317
 
 import java.util.Scanner;
 
@@ -9,30 +9,11 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int dice1 = sc.nextInt();
-        int dice2 = sc.nextInt();
-        int dice3 = sc.nextInt();
+        int dan = sc.nextInt();
 
-        int prize = 0;
-
-        if (dice1 == dice2 && dice1 == dice3) {
-            prize = 10000 + dice1 * 1000;
-        } else if (dice1 == dice2 || dice2 == dice3 || dice1 == dice3) {
-            if (dice1 == dice3) {
-                prize = 1000 + dice3 * 100;
-            } else {
-                prize = 1000 + dice2 * 100;
-            }
-        } else {
-            if (dice1 > dice2 && dice1 > dice3){
-                prize = dice1 * 100;
-            } else if (dice2 > dice1 && dice2 > dice3) {
-                prize = dice2 * 100;
-            } else if (dice3 > dice1 && dice3 > dice2) {
-                prize = dice3 * 100;
-            }
+        for (int i = 1; i < 10; i++){
+            System.out.printf("%d * %d = %d\n", dan, i , dan*i);
         }
-        System.out.println(prize);
 
     }
 }
