4c16eb2e09782e5913df92e80912cf894cc2c635
yunzivv <yunzivv@gmail.com>
2025-03-17 11:35:48 +0900

    Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,30 @@
 
 package org.example;
 
-// HashMap 250317
+// Scanner 250317
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // 배열의 단점 : 크기를 미리 설정해야해서 길이를 늘리거나 수정이 불가
-        // ArrayList의 단점 : 요소 삽입, 삭제 시 인덱스 번호가 달라진다.
+        Scanner sc = new Scanner(System.in);
 
-        // hashmap의 장점 : 키와 밸류를 쌍으로 저장할 수 있다. 1:1 대응이 가능하다.
-        // ArrayList와 같이 제네릭을 지정할 수 있다.
+        int a = 0;
+        while(true) {
 
-        // ArrayList -> 객체를 저장할 때 쉬움 / 뺄 때 귀찮음
-        // Hashmap -> 객체를 저장할 때 귀찮음 / 뺄 때 쉬움
-        //     키(Key), 밸류(Value)
-        HashMap<String, Integer> ageMap = new HashMap();
+            try {
+                a = sc.nextInt(); // 프로그램을 실행하면 정수를 입력하고 엔터를 누를 때 까지 프로세스는 대기
+                sc.nextLine(); // 버퍼를 비우기 위한 용도
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("다시 숫자로 써주라");
+                sc.nextLine(); // 버퍼를 비우기 위한 용도
+            }
+        }
 
-        ageMap.put("케로로나이", 100);
-        ageMap.put("쿠루루나이", 100);
-        ageMap.put("도로로나이", 100);
-
-        System.out.println("케로로 나이 : " + ageMap.get("케로로나이"));
+        System.out.println(a);
 
     }
 }
