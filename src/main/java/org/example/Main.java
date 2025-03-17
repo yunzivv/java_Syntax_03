e2c74b0cebfc2a8d691cde5151d9c1500c2db089
yunzivv <yunzivv@gmail.com>
2025-03-17 12:38:44 +0900

    백준10430

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 
 package org.example;
 
-// 백준 18108 250317
+// 백준 10430 250317
 
 import java.util.Scanner;
 
@@ -9,9 +9,14 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int A = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        System.out.println(A - 543);
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
 
     }
 }
