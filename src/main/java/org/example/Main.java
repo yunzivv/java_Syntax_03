56682cafcf3544b7f471633308dd96dddd145ad8
yunzivv <yunzivv@gmail.com>
2025-03-17 12:21:35 +0900

    백준1000

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 
 package org.example;
 
-// Scanner 250317
+// 백준 1000 250317
 
 import java.util.HashMap;
 import java.util.InputMismatchException;
@@ -11,20 +11,10 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = 0;
-        while(true) {
+        int A = sc.nextInt();
+        int B = sc.nextInt();
 
-            try {
-                a = sc.nextInt(); // 프로그램을 실행하면 정수를 입력하고 엔터를 누를 때 까지 프로세스는 대기
-                sc.nextLine(); // 버퍼를 비우기 위한 용도
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("다시 숫자로 써주라");
-                sc.nextLine(); // 버퍼를 비우기 위한 용도
-            }
-        }
-
-        System.out.println(a);
+        System.out.println(A + B);
 
     }
 }
