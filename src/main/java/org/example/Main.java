c0839e4ed632037b7f7bb6e524e8b5655c6b698e
yunzivv <yunzivv@gmail.com>
2025-03-17 13:10:43 +0900

    백준2588

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 
 package org.example;
 
-// 백준 10430 250317
+// 백준 2588 250317
 
 import java.util.Scanner;
 
@@ -11,12 +11,16 @@ class Main {
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int c = sc.nextInt();
 
-        System.out.println((a + b) % c);
-        System.out.println(((a % c) + (b % c)) % c);
-        System.out.println((a * b) % c);
-        System.out.println(((a % c) * (b % c)) % c);
+        int hundred = (b / 100); // 100의 자리수 구하기
+        int ten = ((b - hundred*100) / 10); // 10의 자리수 구하기
+        int one = b - hundred * 100 - ten * 10; // 1의 자리수 구하기
+
+        System.out.println(a * one);
+        System.out.println(a * ten);
+        System.out.println(a * hundred);
+        System.out.println(a * b);
+
 
     }
 }
