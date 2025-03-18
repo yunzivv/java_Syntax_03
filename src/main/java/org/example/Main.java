fbcd09b890a993f8ffc79ac96285301249623d69
yunzivv <yunzivv@gmail.com>
2025-03-18 10:03:29 +0900

    여러가지stream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,64 @@
 
 package org.example;
 
-// stream, parse 250317
+// 여러가지 stream 250317
 
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import java.util.stream.IntStream;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // 일반
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
 
-        String[] input = sc.nextLine().split(" ");
+        // 스트림
+        IntStream.rangeClosed(1, 10)
+                .forEach(e -> {
+                    System.out.println(e);
+                });
 
-        long sum = Arrays.stream(input)
-                // 하나하나 Long으로 변경
-                // e : 요소
-                .mapToLong(e -> Long.parseLong(e))
-                .sum();
+        // 스트림
+        IntStream.rangeClosed(1, 10)
+                .forEach(e -> System.out.println(e));
 
-//        long sum = 0;
-//
-//        for (String inputData : input) {
-//            sum += Long.parseLong((inputData))
-//        }
+        // 스트림
+        IntStream.rangeClosed(1, 10)
+                .forEach(System.out::println);
+    }
+}
 
-        System.out.println(sum);
+class Main2 {
+    public static void main(String[] args) {
+        // 일반
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] *= 2;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        // 스트림
+        arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int[] resultArr = Arrays.stream(arr)
+                .map(e -> {
+                    return e * 2;
+                })
+                .toArray();
+
+        resultArr = Arrays.stream(arr)
+                .map(e -> e * 2) // 이런식으로 줄여쓸 수 있다.
+                .toArray();
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(resultArr));
     }
 }
