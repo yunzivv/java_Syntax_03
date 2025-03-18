889ccb7fa661c6585fb7af6b74f6c4c920cd6f59
yunzivv <yunzivv@gmail.com>
2025-03-18 10:29:00 +0900

    stream,filter,map2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,55 +1,47 @@
 
 package org.example;
 
-// stream, filter, map 사용해서 홀수 걸러내기 250317
+// stream, filter, map 사용해서 홀수 걸러내기2 250317
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
 class Main {
     public static void main(String[] args) {
-
         // 일반
         int[] arr = {33, 2, 55, 4, 51, 6, 71, 18, 29, 10};
 
+        List<Integer> resultAl = new ArrayList<>();
+
         // filter 시작
-        int resultArrLen = 0;
-
-        for (int n : arr) if (n % 2 == 0) resultArrLen++;
-
-        int[] resultArr = new int[resultArrLen];
-
-        int resultArrLastIndex = -1;
-
-        for (int n : arr) {
-            if (n % 2 != 0) continue;
-
-            resultArr[++resultArrLastIndex] = n;
-        }
+        for (int n : arr) if (n % 2 == 0) resultAl.add(n);
         // filter 끝
 
         // map 시작
-        for (int i = 0; i < resultArr.length; i++) {
-            resultArr[i] *= 2;
+        for (int i = 0; i < resultAl.size(); i++) {
+            int newValue = resultAl.get(i) * 2;
+            resultAl.set(i, newValue);
         }
         // map 끝
 
-        System.out.println(Arrays.toString(resultArr));
+        System.out.println(resultAl);
 
         // 스트림
         arr = new int[]{33, 2, 55, 4, 51, 6, 71, 18, 29, 10};
 
-        resultArr = Arrays.stream(arr)
+        resultAl = Arrays
+                .stream(arr)
                 .filter(e -> e % 2 == 0)
                 .map(e -> e * 2)
-                .toArray();
+                .boxed()
+                .collect(Collectors.toList());
 
-        System.out.println(Arrays.toString(resultArr));
-
+        System.out.println(resultAl);
     }
 }
-
