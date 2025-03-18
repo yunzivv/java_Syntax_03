d7131290687230a197a38aabb7531da53fa91365
yunzivv <yunzivv@gmail.com>
2025-03-18 09:12:52 +0900

    백준10950

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,31 @@ package org.example;
 
 // 백준 2439 250317
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int starline = sc.nextInt();
+        List<Integer> numbers = new ArrayList<>();
+        int lastindex = 0;
 
-        for (int i = 1; i <= starline; i++){
-            for (int space = starline - 1; space >= i; space--){
-                System.out.print(" ");
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            numbers.add(a);
+            numbers.add(b);
+            if (a == 0 && b == 0) {
+                break;
             }
-            for (int star = 1; star <= i; star++){
-                System.out.print("*");
-            }
-            System.out.println("");
+        }
+        int size = numbers.size();
+        for (int i = 0; i < (numbers.size() - 2) / 2; i++) {
+            int sum = numbers.get(lastindex) + numbers.get(lastindex + 1);
+            System.out.println(sum);
+            lastindex += 2;
         }
     }
 }
