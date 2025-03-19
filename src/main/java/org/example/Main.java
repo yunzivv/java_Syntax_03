bb36cf7ad160ff6c41d7133094552206b283b5a2
yunzivv <yunzivv@gmail.com>
2025-03-19 23:33:01 +0900

    백준10871

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ class Main {
         Scanner sc = new Scanner(System.in);
 
         int all = sc.nextInt();
+        int standard = sc.nextInt();
         int[] numbers = new int[all];
         int count = 0;
 
@@ -17,12 +18,10 @@ class Main {
             numbers[i] = sc.nextInt();
         }
 
-        int check = sc.nextInt();
         for(int i = 0; i < all; i++){
-            if(numbers[i] == check){
-                count++;
+            if(numbers[i] < standard){
+                System.out.println(numbers[i]);
             }
         }
-        System.out.println(count);
     }
 }
