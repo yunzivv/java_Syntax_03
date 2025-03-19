5a9e36f2cc258ef8708521474461afdb826c05a3
yunzivv <yunzivv@gmail.com>
2025-03-19 23:24:45 +0900

    백준10807

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,28 @@
 
 package org.example;
 
-// 백준 15552 250319
+// 백준 10807 250319
 
-
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        // 버퍼는
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 버퍼를 활용한 입력
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 버퍼를 활용한 출력
+        int all = sc.nextInt();
+        int[] numbers = new int[all];
+        int count = 0;
 
-        // 문자열을 공백 단위로 구분하여 순서대로 호출한다.
-        StringTokenizer str;
-
-        // readLine() 메서드는 단 한 줄만 읽을 수 있고 개행문자만을 종료로 인지한다.
-        int testcase = Integer.parseInt(bf.readLine());
-        for (int i = 0; i < testcase; i++) {
-            str = new StringTokenizer(bf.readLine());
-            bw.write((Integer.parseInt(str.nextToken()) + Integer.parseInt(str.nextToken())) + "\n");
+        for (int i = 0; i < all; i++){
+            numbers[i] = sc.nextInt();
         }
-        bw.close();
+
+        int check = sc.nextInt();
+        for(int i = 0; i < all; i++){
+            if(numbers[i] == check){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
