c07de4e2ef9cb1fd1e18b89b1ad013ac6ed69444
yunzivv <yunzivv@gmail.com>
2025-03-19 23:08:08 +0900

    백준15552

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,30 @@
 
 package org.example;
 
-// stream, filter, map 사용해서 홀수 걸러내기2 250317
+// 백준 15552 250319
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
+import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
-        // 일반
-        int[] arr = {33, 2, 55, 4, 51, 6, 71, 18, 29, 10};
+    public static void main(String[] args) throws IOException {
 
-        List<Integer> resultAl = new ArrayList<>();
+        // 버퍼는
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 버퍼를 활용한 입력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 버퍼를 활용한 출력
 
-        // filter 시작
-        for (int n : arr) if (n % 2 == 0) resultAl.add(n);
-        // filter 끝
+        // 문자열을 공백 단위로 구분하여 순서대로 호출한다.
+        StringTokenizer str;
 
-        // map 시작
-        for (int i = 0; i < resultAl.size(); i++) {
-            int newValue = resultAl.get(i) * 2;
-            resultAl.set(i, newValue);
+        // readLine() 메서드는 단 한 줄만 읽을 수 있고 개행문자만을 종료로 인지한다.
+        int testcase = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < testcase; i++) {
+            str = new StringTokenizer(bf.readLine());
+            bw.write((Integer.parseInt(str.nextToken()) + Integer.parseInt(str.nextToken())) + "\n");
         }
-        // map 끝
-
-        System.out.println(resultAl);
-
-        // 스트림
-        arr = new int[]{33, 2, 55, 4, 51, 6, 71, 18, 29, 10};
-
-        resultAl = Arrays
-                .stream(arr)
-                .filter(e -> e % 2 == 0)
-                .map(e -> e * 2)
-                .boxed()
-                .collect(Collectors.toList());
-
-        System.out.println(resultAl);
+        bw.close();
     }
 }
