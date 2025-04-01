d0dda4fa1945d217e8d6bbdf18a41ae4e9f12a63
yunzivv <yunzivv@gmail.com>
2025-04-01 22:03:16 +0900

    백준2743

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// 백준 3052 250331
+// 백준 2743 250401
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,25 +10,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] numbers = new int[10];
-        for (int i = 0; i < 10; i++) {
-            numbers[i] = Integer.parseInt(sc.nextLine());
-        }
+        String s = sc.nextLine();
 
-        int[] remainder = new int[10];
-        for (int i = 0; i < 10; i++) {
-            remainder[i] = numbers[i] % 42;
-        }
-        Arrays.sort(remainder);
-
-        int countRemainder = 1;
-        for (int i = 0; i < 9; i++) {
-            if(remainder[i] != remainder[i+1])  {
-                countRemainder++;
-            }
-
-        }
-        System.out.println(countRemainder);
+        System.out.println(s.length());
 
         sc.close();
     }
