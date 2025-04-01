f67dafdf2569eb3e63f164f3531c4088df5369e7
yunzivv <yunzivv@gmail.com>
2025-04-01 22:12:50 +0900

    백준 11654

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// 백준 9086 250401
+// 백준 11654 250401
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,17 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        sc.nextLine();
-        String[] str = new String[n];
-        for (int i = 0; i < n; i++) {
-            str[i] = sc.nextLine();
-        }
-        for (int i = 0; i < n; i++) {
-            int len = str[i].length() - 1;
-            System.out.print(str[i].charAt(0));
-            System.out.println(str[i].substring(len));
-        }
+        char c = sc.next().charAt(0);
+        System.out.println((int)c);
 
         sc.close();
     }
